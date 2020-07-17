33c33fe6483e88584cc7c98919da4ca9ebb324d9
SungJaeLee180 <goodsj9402@gmail.com>
2020-07-17 16:49:57 +0900

    adder func seperation

---
+++ adder/add_p.py
@@ -0,0 +1,10 @@
+def addfunc(iter):
+    sum_ = 0
+    for i in iter:
+        sum_ += i
+    return sum_
+
+if __name__ == '__main__':
+    print('test start')
+    assert(addfunc([2, 4, 5] == 11))
+    print('pass')
